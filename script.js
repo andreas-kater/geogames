3402dc540771fe390fa1165a29670fa9b3d29b6c
Andreas Kater <andreaskater@andi-mac-book-pro.local>
2019-02-09 10:45:58 +0000

    can show map of world

--- script.js
+++ script.js
@@ -1,69 +1,13 @@
-var mymap = L.map('mapid').setView([2.2655324, 23.5914664], 4);
-L.tileLayer.provider('CartoDB.PositronNoLabels').addTo(mymap);
+var map = L.map('mapid').setView([2.2655324, 23.5914664], 4);
+// L.tileLayer.provider('CartoDB.PositronNoLabels').addTo(map);
 
+var geojsonLayer = new L.GeoJSON.AJAX("assets/world110m.geojson");
+geojsonLayer.addTo(map);
 
-var someFeatures = [{
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "show_on_map": true
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
-    }
-}, {
-    "type": "Feature",
-    "properties": {
-        "name": "Busch Field",
-        "show_on_map": false
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.98404, 39.74621]
-    }
-}];
-
-L.geoJSON(someFeatures, {
-    filter: function(feature, layer) {
-        return feature.properties.show_on_map;
-    }
-}).addTo(mymap);
-
-
-var states = [{
-    "type": "Feature",
-    "properties": {"party": "Republican"},
-    "geometry": {
-        "type": "Polygon",
-        "coordinates": [[
-            [-104.05, 48.99],
-            [-97.22,  48.98],
-            [-96.58,  45.94],
-            [-104.03, 45.94],
-            [-104.05, 48.99]
-        ]]
-    }
-}, {
-    "type": "Feature",
-    "properties": {"party": "Democrat"},
-    "geometry": {
-        "type": "Polygon",
-        "coordinates": [[
-            [-109.05, 41.00],
-            [-102.06, 40.99],
-            [-102.03, 36.99],
-            [-109.04, 36.99],
-            [-109.05, 41.00]
-        ]]
-    }
-}];
-
-L.geoJSON(states, {
-    style: function(feature) {
-        switch (feature.properties.party) {
-            case 'Republican': return {color: "#ff0000"};
-            case 'Democrat':   return {color: "#0000ff"};
-        }
-    }
-}).addTo(mymap);
+map.on('click', function (e) {
+  var popLocation = e.latlng;
+  var popup = L.popup()
+    .setLatLng(popLocation)
+    .setContent('<p>Hello world!<br />This is a nice popup.</p>')
+    .openOn(map);
+});
